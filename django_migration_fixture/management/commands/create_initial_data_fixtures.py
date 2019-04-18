67990f64b042e90f1777dbe0be7aca1922f56390
alex.tarasuk <alex.tarasuk@flyaps.com>
2019-04-18 13:33:22 +0300

    Support of passing fixture name. Refactoring

--- django_migration_fixture/management/commands/create_initial_data_fixtures.py
+++ django_migration_fixture/management/commands/create_initial_data_fixtures.py
@@ -1,45 +1,55 @@
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-import glob
 import os
-import re
+import glob
+
+from io import StringIO
 
 from django.apps import apps
 from django.core import management
-from django.core.management import BaseCommand
-from django.core.management.base import CommandError
 from django.db.migrations import writer
+from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
-    help = "Locate initial_data.* files and create data migrations for them."
+    help = "Locate files and create data migrations for them."
 
     def handle(self, *args, **options):
-        # Loop through all apps
+        fixture_file = options['file'] if options['file'] else 'initial_data'
+        fixture_template = f"{fixture_file}.*"
+        applied_number = 0
+
         for app in apps.get_app_configs():
-            # Look for initial_data.* files
-            for fixture_path in glob.glob(os.path.join(app.path, 'fixtures', 'initial_data.*')):
-                # Ensure the app has at least an initial migration
+            for fixture_path in glob.glob(os.path.join(app.path, 'fixtures', fixture_template)):
                 if not glob.glob(os.path.join(app.path, 'migrations', '0001*')):
-                    self.stdout.write(self.style.MIGRATE_HEADING("Migrations for '%s':" % app.label) + "\n")
-                    self.stdout.write("  %s\n" % (self.style.WARNING("Ignoring '%s' - not migrated." % os.path.basename(fixture_path)),))
+                    self.stdout.write(self.style.MIGRATE_HEADING(f"Migrations for '{app.label}':\n"))
+                    self.stdout.write(
+                        self.style.WARNING(f"Ignoring '{os.path.basename(fixture_path)}' - not migrated.\n")
+                    )
+                    self.stdout.write(
+                        self.style.WARNING('There are no migrations at all\n')
+                    )
                     continue
 
                 if self.migration_exists(app, fixture_path):
-                    self.stdout.write(self.style.MIGRATE_HEADING("Migrations for '%s':" % app.label) + "\n")
-                    self.stdout.write("  %s\n" % (self.style.NOTICE("Ignoring '%s' - migration already exists." % os.path.basename(fixture_path)),))
+                    self.stdout.write(self.style.MIGRATE_HEADING(f"Migrations for '{app.label}':\n"))
+                    self.stdout.write(
+                        self.style.NOTICE(
+                            f"Ignoring '{os.path.basename(fixture_path)}' - migration already exists.\n"
+                        )
+                    )
                     continue
 
-                # Finally create our data migration
                 self.create_migration(app, fixture_path)
+                applied_number += 1
+
+        self.stdout.write(
+            self.style.SUCCESS(f'{applied_number} fixtures has been loaded')
+        )
 
     def monkey_patch_migration_template(self, app, fixture_path):
         """
         Monkey patch the django.db.migrations.writer.MIGRATION_TEMPLATE
 
-        Monkey patching django.db.migrations.writer.MIGRATION_TEMPLATE means that we 
+        Monkey patching django.db.migrations.writer.MIGRATION_TEMPLATE means that we
         don't have to do any complex regex or reflection.
 
         It's hacky... but works atm.
@@ -55,18 +65,16 @@ class Command(BaseCommand):
                 module_split[-1:][0],
             )
 
-        writer.MIGRATION_TEMPLATE = writer.MIGRATION_TEMPLATE\
-            .replace(
-                '%(imports)s',
-                "%(imports)s" + "\nfrom django_migration_fixture import fixture\n%s" % module_import
-            )\
-            .replace(
-                '%(operations)s', 
-                "        migrations.RunPython(**fixture(%s, ['%s'])),\n" % (
-                    app.label,
-                    os.path.basename(fixture_path)
-                ) + "%(operations)s\n" 
-            )
+        writer.MIGRATION_TEMPLATE = writer.MIGRATION_TEMPLATE.replace(
+            '%(imports)s',
+            "%(imports)s" + "\nfrom django_migration_fixture import fixture\n%s" % module_import
+        ).replace(
+            '%(operations)s',
+            "        migrations.RunPython(**fixture(%s, ['%s'])),\n" % (
+                app.label,
+                os.path.basename(fixture_path)
+            ) + "%(operations)s\n"
+        )
 
     def restore_migration_template(self):
         """
@@ -95,5 +103,7 @@ class Command(BaseCommand):
         management.call_command('makemigrations', app.label, empty=True, stdout=out)
 
         self.restore_migration_template()
-
         self.stdout.write(out.getvalue())
+
+    def add_arguments(self, parser):
+        parser.add_argument('-f', '--file', type=str)
